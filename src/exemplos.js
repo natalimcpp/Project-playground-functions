7c037b2daf4f275082a3eab1bdfbd8f3c22377a6
natalimcpp <natalimcpp@gmail.com>
2023-10-17 20:10:37 -0300

    Desafio 2: cria a função que calcula a área de um triângulo

--- src/exemplos.js
+++ src/exemplos.js
@@ -13,3 +13,33 @@ let ex1= 4*5;
 let ex2= 41+20;
 
 console.log(compareTrue(ex1, ex2));
+
+//Desafio 2 
+
+function calcArea(base, height) {
+    let area = (base * height / 2);
+    return area;
+}
+
+let base = 10
+let height = 50
+
+console.log(calcArea(base, height)); 
+
+//Desafio 2 com listas
+
+function calcArea(base, height) {
+    let areas = [];
+
+    for (let i = 0; i < base.length; i++) {
+        areas.push((base[i] * height[i]) / 2);
+    }
+
+    return areas;
+}
+
+let bases = [10, 5, 51];
+let heights = [50, 2, 1];
+
+let result = calcArea(bases, heights);
+console.log(result);
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,15 +1,15 @@
-function compareTrue(a, b) {
-    if (a == 20 && b == 60) {
-        return console.log("verdadeiro");
-    } 
-    
-    {
-        console.log("falso");
+function calcArea(base, height) {
+    let areas = [];
+
+    for (let i = 0; i < base.length; i++) {
+        areas.push((base[i] * height[i]) / 2);
     }
 
-} 
+    return areas;
+}
 
-let ex1= 4*5;
-let ex2= 41+20;
+let bases = [10, 5, 51];
+let heights = [50, 2, 1];
 
-console.log(compareTrue(ex1, ex2));
+let result = calcArea(bases, heights);
+console.log(result);
