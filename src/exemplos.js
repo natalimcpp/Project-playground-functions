31634b1eb73f37f10f5c1cd869aa0108d7cc0b5a
natalimcpp <natalimcpp@gmail.com>
2023-10-17 16:56:43 -0300

    Adicione o arquivo exemplos.js

---
+++ src/exemplos.js
@@ -0,0 +1,15 @@
+//Desafio 1
+
+function compareTrue(a, b) {
+    if (a == 20 && b == 60) {
+        return console.log("verdadeiro");
+    } else {
+        console.log("falso");
+        }
+
+} 
+
+let ex1= 4*5;
+let ex2= 41+20;
+
+console.log(compareTrue(ex1, ex2));
