ab00c61091a2e171fdfba47736dee070f8396aaa
natalimcpp <natalimcpp@gmail.com>
2023-10-24 21:38:16 -0300

    Desafio 8: Crie uma função FizzBuz

--- src/exemplos.js
+++ src/exemplos.js
@@ -150,3 +150,38 @@ function catAndMouse(x, y, z) {
 }
 
 console.log (catAndMouse(5, 1, 8));
+console.log (catAndMouse(8, 2, 20));
+console.log (catAndMouse(5, 3, 7));
+
+//Desafio 8
+
+function fizzBuzz(arr) {
+
+    let arrResultado = [];
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] % 3 === 0 && arr[i] % 5 === 0) {
+            arrResultado.push('fizzBuzz')
+        } else if (arr[i] % 3 === 0) {
+            arrResultado.push('fizz')
+        } else if (arr[i] % 5 === 0) {
+            arrResultado.push('buzz')
+        } else {
+            arrResultado.push('bug!')
+        }
+    }
+
+    return arrResultado;
+}
+
+let array1 = [2, 15, 7, 9, 45]
+console.log(fizzBuzz(array1))
+
+let array2 = [7, 9]
+console.log(fizzBuzz(array2))
+
+let array3 = [9, 25]
+console.log(fizzBuzz(array3))
+
+
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,19 +1,28 @@
-function catAndMouse(x, y, z) {
-    let mouse = x;
-    let cat1 = y;
-    let cat2 = z;
+function fizzBuzz(arr) {
 
-    if ((Math.abs(mouse-cat1)) < (Math.abs(mouse - cat2))) {
-        return "cat 1"
-    } else if ((Math.abs(mouse-cat1)) > (Math.abs(mouse - cat2))) {
+    let arrResultado = [];
 
-        return "cat 2"         
-    } else {
+    for (let i = 0; i < arr.length; i++) {
 
-        return "os gatos trombam e o gato foge"
+        if (arr[i] % 3 === 0 && arr[i] % 5 === 0) {
+            arrResultado.push('fizzBuzz')
+        } else if (arr[i] % 3 === 0) {
+            arrResultado.push('fizz')
+        } else if (arr[i] % 5 === 0) {
+            arrResultado.push('buzz')
+        } else {
+            arrResultado.push('bug!')
+        }
     }
+
+    return arrResultado;
 }
 
-console.log (catAndMouse(5, 1, 8));
-console.log (catAndMouse(8, 2, 20));
-console.log (catAndMouse(5, 3, 7));
+let array1 = [2, 15, 7, 9, 45]
+console.log(fizzBuzz(array1))
+
+let array2 = [7, 9]
+console.log(fizzBuzz(array2))
+
+let array3 = [9, 25]
+console.log(fizzBuzz(array3))
